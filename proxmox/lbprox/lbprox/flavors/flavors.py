cbbfeb649162733feadf8bbbef898605e1013588
Yogev Cohen <yogev@lightbitslabs.com>
2024-12-24 12:49:01 +0200

    chore(flavors): change flavor.yaml embedded struct syntax.

--- proxmox/lbprox/lbprox/flavors/flavors.py
+++ proxmox/lbprox/lbprox/flavors/flavors.py
@@ -6,4 +6,5 @@ CONFIG_DIRECTORY = "lbprox/config"
 
 def list_machine_types():
     machines_file = os.path.join(CONFIG_DIRECTORY, "flavors/flavors.yml")
-    return yaml.load(open(machines_file, "r"), Loader=yaml.FullLoader)
+    with open(machines_file, 'r') as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
